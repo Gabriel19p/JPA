7d7f49c62849c3ef0abfb8ff00cb71ae00f48ee8
Gabriel Freitas <122943129+Gabriel19p@users.noreply.github.com>
2023-05-22 18:49:38 -0300

    Update Employee.java

--- jpaMapeamentos_n_n/src/main/java/jpaMapeamentos_n_n/Employee.java
+++ jpaMapeamentos_n_n/src/main/java/jpaMapeamentos_n_n/Employee.java
@@ -1,32 +1,30 @@
 package jpaMapeamentos_n_n;
 
-import java.util.List;
+import java.util.List; 
 import java.util.Set;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
 
-@Entity /* criacao da tabela */
+@Entity
 public class Employee {
 
-	@Id /* chave primaria */
-	@GeneratedValue(strategy = GenerationType.IDENTITY) /* auto incrementa por responsabilidade do bd */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int eid;
 	private String ename;
 	private double salary;
 	private String deg;
 
 	@ManyToMany
-	@JoinTable(name = "employee_department", joinColumns = @JoinColumn(name = "employee_id"), inverseJoinColumns = @JoinColumn(name = "department_id"))
+	@JoinTable(name = "Employee_department", joinColumns = @JoinColumn (name = "employee_eid"), inverseJoinColumns = @JoinColumn(name = "department_id"))
 
-	private List<Department> departmentSet;
-
-	public Employee(int eid, String ename, double salary, String deg, List<Department> departmentSet) {
+	public Employee(int eid, String ename, double salary, String deg, Set<Department> departmentSet) {
 		super();
 		this.eid = eid;
 		this.ename = ename;
@@ -39,44 +37,25 @@ public class Employee {
 		super();
 	}
 
-	public int getEid() {
-		return eid;
+	public void setEname(String string) {
+		// TODO Auto-generated method stub
+		
 	}
 
-	public void setEid(int eid) {
-		this.eid = eid;
+	public void setSalary(double d) {
+		// TODO Auto-generated method stub
+		
 	}
 
-	public String getEname() {
-		return ename;
+	public void setDeg(String string) {
+		// TODO Auto-generated method stub
+		
 	}
 
-	public void setEname(String ename) {
-		this.ename = ename;
+	public void setDepartmentSet(List<Department> departmentSet1) {
+		// TODO Auto-generated method stub
+		
 	}
 
-	public double getSalary() {
-		return salary;
-	}
-
-	public void setSalary(double salary) {
-		this.salary = salary;
-	}
-
-	public String getDeg() {
-		return deg;
-	}
-
-	public void setDeg(String deg) {
-		this.deg = deg;
-	}
-
-	public List<Department> getDepartmentSet() {
-		return departmentSet;
-	}
-
-	public void setDepartmentSet(List<Department> departmentSet) {
-		this.departmentSet = departmentSet;
-	}
-
+	// Getters and setters
 }
